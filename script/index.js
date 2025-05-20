31e66272070ca8e90f8122664d0bf3548609b97f
DipuBaroi <dipubaroi758@gmail.com>
2025-05-20 20:32:49 +0600

    added show details button and verify img

--- script/index.js
+++ script/index.js
@@ -1,45 +1,74 @@
-function removeActiveClass(){
+function removeActiveClass() {
     const activeButtons = document.getElementsByClassName('active')
-    
-    for(const btn of activeButtons){
+
+    for (const btn of activeButtons) {
         btn.classList.remove('active')
     }
 }
 
-function loadCategories(){
+function loadCategories() {
     // fetch the data
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    // convert promise
-    .then(res => res.json())
-    // send data 
-    .then(data=>displayCategories(data.categories
-))
+        // convert promise
+        .then(res => res.json())
+        // send data 
+        .then(data => displayCategories(data.categories
+        ))
 }
 
-function loadVideo (){
+function loadVideo() {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(res => res.json())
-    .then(data=>{
-        document.getElementById('btn-all').classList.add('active')
-        displayVideos(data.videos)
-    })
+        .then(res => res.json())
+        .then(data => {
+            removeActiveClass();
+            document.getElementById('btn-all').classList.add('active')
+            displayVideos(data.videos)
+        })
 }
 
-const loadCategoryVideos = (id) =>{
-    url =`
+const loadCategoryVideos = (id) => {
+    url = `
     https://openapi.programming-hero.com/api/phero-tube/category/${id}
     `
     console.log(url);
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        removeActiveClass()
-        const clickedButton = document.getElementById(`btn-${id}`)
-         console.log(clickedButton)
-         clickedButton.classList.add('active')
-        displayVideos(data.category)
-       
-    })
+        .then(res => res.json())
+        .then(data => {
+            removeActiveClass()
+            const clickedButton = document.getElementById(`btn-${id}`)
+            console.log(clickedButton)
+            clickedButton.classList.add('active')
+            displayVideos(data.category)
+
+        })
+}
+
+const loadVideoDetails = (videoId) => {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayVideoDetails(data.video))
+}
+
+const displayVideoDetails = (video) => {
+    console.log(video)
+    document.getElementById('video-details').showModal();
+    const detailsContainer = document.getElementById('details-container');
+
+    detailsContainer.innerHTML = `
+        <div class="card bg-base-100 image-full shadow-xl">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description} </p>
+    
+  </div>
+</div>
+    `
 }
 
 // {
@@ -48,22 +77,22 @@ const loadCategoryVideos = (id) =>{
 // }
 
 
-function displayCategories(categories){
+function displayCategories(categories) {
     // get container
     const categoryContainer = document.getElementById('category-container')
 
     // loop operation on array of object
 
-    for(const cat of categories){
-        
+    for (const cat of categories) {
 
-    // creat the element
+
+        // creat the element
         const categoryDiv = document.createElement('div');
-        categoryDiv.innerHTML =`
+        categoryDiv.innerHTML = `
         <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id} )" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
         `
-    // append the element
-    categoryContainer.appendChild(categoryDiv)
+        // append the element
+        categoryContainer.appendChild(categoryDiv)
     }
 }
 
@@ -86,29 +115,29 @@ function displayCategories(categories){
 //     "description": "Ethan Clark's 'Colors of the Wind' is a vibrant musical exploration that captivates listeners with its rich, expressive melodies and uplifting rhythm. With 233K views, this song is a celebration of nature's beauty and human connection, offering a soothing and enriching experience for fans of heartfelt, nature-inspired music."
 // }
 
-const displayVideos = (videos) =>{
+const displayVideos = (videos) => {
     // get container
     const videoContainer = document.getElementById('video-container');
 
-    
+
     videoContainer.innerHTML = '';
 
-    if(videos.length === 0){
-        videoContainer.innerHTML=`
+    if (videos.length === 0) {
+        videoContainer.innerHTML = `
         <div class="py-20 col-span-4 flex flex-col items-center justify-center">
             <img src="assets/Icon.png" alt="">
             <h2 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
         </div>
         `
-        
+
         return
     }
     // forEach()
-    videos.forEach(video=>{
+    videos.forEach(video => {
         // console.log(video)
 
         const videoCard = document.createElement('div')
-        videoCard.innerHTML=`
+        videoCard.innerHTML = `
      <div class="card card-compact bg-base-100 ">
                 <!-- thumbnail -->
                 <figure class="relative">
@@ -127,16 +156,19 @@ const displayVideos = (videos) =>{
                     </div>
                     <div class="intro">
                         <h3 class="font-semibold text-sm">${video.title} </h3>
-                        <p class="text-sm text-gray-500 flex gap-3">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" alt=""></p>
+                        <p class="text-sm text-gray-500 flex gap-3">
+                        ${video.authors[0].profile_name} 
+                        ${video.authors[0].verified == true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" alt=""></p>` : ``} 
                         <p class="text-sm text-gray-500">${video.others.views} views</p>
                     </div>
                 </div>
+                <button onclick=loadVideoDetails('${video.video_id}') class="btn btn-block">Show Details</button>
             </div>
         `
         // append
         videoContainer.appendChild(videoCard)
     })
-    
+
 }
 
 loadCategories()
